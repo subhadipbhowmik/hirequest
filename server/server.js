455ae0f628359fa11c998927d2cf38749f8a6e4d
Shubhadip Bhowmik <shubhadipbhowmik02@gmail.com>
2025-04-01 00:26:24 +0530

    Price Updated

--- server/server.js
+++ server/server.js
@@ -8,7 +8,14 @@ dotenv.config();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://hirequest-cu.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // connect to db
 connectDB();
